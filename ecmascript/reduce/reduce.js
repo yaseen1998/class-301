a1cab123d68dac6ba78a6b2c3ff53ac2020e69fd
yaseen1998 <yasseenkouthe@eng.hu.edu.jo>
2021-09-10 22:54:28 +0300

    ddd

--- ecmascript/reduce/reduce.js
+++ ecmascript/reduce/reduce.js
@@ -1,44 +1,51 @@
-// let num =[10,20,15,30]
-// let add =num.reduce(function(acc,current,index,arr){
-//     console.log(acc);
-//     console.log(index);
-//     console.log(current);
-//     console.log(arr);
-//     return acc + current
-// })
-// console.log(add);
+let num =[10,20,15,30]
+let add =num.reduce(function(acc,current,index,arr){
+    console.log(acc);
+    console.log(index);
+    console.log(current);
+    console.log(arr);
+    return acc + current
+})
+console.log(add);
 
-// let add2 =num.reduce(function(acc,current,index,arr){
-//     console.log(acc);
-//     console.log(index);
-//     console.log(current);
-//     console.log(arr);
-//     return acc + current
-// },5)
-// console.log(add2);
+let add2 =num.reduce(function(acc,current,index,arr){
+    console.log(acc);
+    console.log(index);
+    console.log(current);
+    console.log(arr);
+    return acc + current
+},5)
+console.log(add2);
 
 
-// let bigger = ['bla','aaaf','gafadfa','agaff']
-// let check =bigger.reduce(function(acc,current,index,arr){
-//     console.log(acc);
-//     console.log(index);
-//     console.log(current);
-//     console.log(arr);
-//     console.log(acc.length > current.length ? acc:current);
-//     return acc.length > current.length ? acc:current
-// })
-// console.log(check);
+let bigger = ['bla','aaaf','gafadfa','agaff']
+let check =bigger.reduce(function(acc,current,index,arr){
+    console.log(acc);
+    console.log(index);
+    console.log(current);
+    console.log(arr);
+    console.log(acc.length > current.length ? acc:current);
+    return acc.length > current.length ? acc:current
+})
+console.log(check);
 
-// let remove = ['e','s','r','e']
-// let final =remove.filter(function(ele){
-//     return !ele.startsWith('e')
-// }).reduce(function(acc,current){
-//     return `${acc}${current}`
-// })
-// console.log(final);
+let remove = ['e','s','r','e']
+let final =remove.filter(function(ele){
+    return !ele.startsWith('e')
+}).reduce(function(acc,current){
+    return `${acc}${current}`
+})
+console.log(final);
 
 let remove2 = ['e','s','r','e']
 let final2 =remove2.filter(function(ele){
     return !ele.startsWith('e')
 })
 console.log(...final2);
+
+var reverseString = function(s) {
+    let d = s.split('')
+    return d.reduce((rev, char) => char+rev, "");
+  }
+  console.log(reverseString('code drft'));
+  
